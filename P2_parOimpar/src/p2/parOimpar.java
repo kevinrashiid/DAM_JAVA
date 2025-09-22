10f89b7f9a5ffefd0550cd4867e1059336e4d61a
alumno <alumno@1DAM-04>
2025-09-22 08:40:42 +0200

    Ediciones desde clase

--- P2_parOimpar/src/p2/parOimpar.java
+++ P2_parOimpar/src/p2/parOimpar.java
@@ -23,8 +23,10 @@ public class parOimpar {
 		//PEDIR DOS NUMERO POR SEPARADO CUAL ES DE LOS DOS ES MAYOR
 		Scanner numero1 = new Scanner(System.in);
 		System.out.println("Escribe dos numero y te dire cual es mayor");
+		
 		System.out.println("Escribe el primer numero: ");
 		int  num1 = numero1.nextInt();
+		
 		Scanner numero2 = new Scanner(System.in);
 		System.out.println("Escribe el segundo numero");
 		int num2 = numero2.nextInt();
