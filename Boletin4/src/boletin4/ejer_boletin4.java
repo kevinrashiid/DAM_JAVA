e2703eff6f20a8dab30dfc4b1a859815e062663f
kevin.rashiid <kevin.villaroelm@gmail.com>
2025-10-09 12:29:09 +0200

    otros

--- Boletin4/src/boletin4/ejer_boletin4.java
+++ Boletin4/src/boletin4/ejer_boletin4.java
@@ -33,13 +33,16 @@ public class ejer_boletin4 {
 		//y ese int lo comparamos con int que pasamos por teclado
 		/*
 		Scanner teclado = new Scanner(System.in);
-		System.out.println("introduce numero y te dire si es CAPICUA");
-		
+		System.out.print("introduce numero y te dire si es CAPICUA --> ");
 		int numeroPedido=teclado.nextInt();
+		teclado.close();//cerramos el teclado
 		
 		String numeroTexto=String.valueOf(numeroPedido);//convertir de numero a texto
-		String alReves ="";
-		for(int i=numeroTexto.length()-1; i>=0; i--) {//recoremos la cadena al reves
+		String alReves ="";//cadena vacia para introducir el texto al reves
+		
+		//recoremos la cadena al reves -1 por que queremos la ultima posicion
+		//y se para cuando sea menor que 0
+		for(int i=numeroTexto.length()-1; i>=0; i--) {
 			alReves = alReves + numeroTexto.charAt(i);//introducimos la cadena al reves en la variable
 		}
 		int numeroInt=Integer.parseInt(alReves);//convertir de texto a numero
@@ -50,14 +53,111 @@ public class ejer_boletin4 {
 		}
 		*/
 		// ================ejercicio 14=============================
-		//Escribe un programa que lea una hora por teclado en formato 24 horas (HH:MM). Tu
-		//programa debería de decir si corresponde a la mañana (entre las 6 y las 11, ambas
+		//Escribe un programa que lea una hora por teclado en formato 24 horas (HH:MM). 
+		//Tu programa debería de decir si corresponde a la mañana (entre las 6 y las 11, ambas
 		//inclusive), si es una hora de la tarde (entre las 12 y las 19, ambas inclusive), si es de la
 		//noche (entre las 20 y las 23, ambas inclusive), si es de la madrugada (entre las 0 y las
 		//5, ambas inclusive) o bien, si el formato no es correcto o no se corresponde con una
 		//hora real (minutos de mas de 60, horas negativas o por encima de 23, etc.
-		Scanner teclado = new Scanner(System.in);
 		
+		//SOLUCION DEL PROFE --VALIDACION DE UNA CADENA 
+		// QUE COSAS TENGO QUE VALIDAR
+		//1.-Longitud==5
+		//2.-Caracter de separacion ==':'
+		//3.-horas entre 0>= <=23
+		//4.- Minutos entre 0>= <=59
+		Scanner teclado = new Scanner(System.in);
+		System.out.print("INTRODUCE HORA EN FORMATO MM:HH --> ");
+		String hora= teclado.nextLine();
+		teclado.close();
+		boolean bandera=true; //bandera
+		int hhEntero=0;
+		if(hora.length()==5)	{
+			if(hora.charAt(2)==':') {
+				
+				/*
+				String hh=hora.substring(0,2);//extraemos las horas
+				String mm=hora.substring(3,5);//extraemos los minutos
+				int hhEntero=Integer.parseInt(hh); //conversion se horas a enteros
+				int mmEntero=Integer.parseInt(mm);//conversion se minutos a enteros
+				*/
+				//ESTO ES LO MISMO QUE LO DE ARRIBA COMENRADO
+				hhEntero=Integer.parseInt(hora.substring(0,2)); //CORTAMOS LAS HORAS Y LAS CONVERTIMOS A INT
+				int mmEntero=Integer.parseInt(hora.substring(3,5)); //CORTAMOS LOS MINUTOS Y LOS CONVERTIMOS A INT
+				if(hhEntero>=0 && hhEntero<=23) {
+					if(mmEntero>=0 && mmEntero<=59) {
+						bandera=true;
+					}
+					else {
+						bandera =false;
+					}
+				}else {
+					bandera =false;
+				}
+			}
+			
+			else {
+				bandera=false;
+			}
+		}
+		else {
+			bandera=false;
+		}
+		if(bandera=true) {
+			System.out.println("la hora " + hora+" esta en un formato correcto");
+			if(hhEntero>=06 && hhEntero<=11)	{
+				System.out.println("Las "+hora+" es por la mañana");
+			}
+			else if(hhEntero>=12 && hhEntero<=19) {
+				System.out.println("Las "+hora+" es por la tarde");
+			}
+			else if(hhEntero>=20 && hhEntero<=23) {
+				System.out.println("Las "+hora+" es por la noche");
+			}
+			else if(hhEntero>=00 && hhEntero<=05) {
+				System.out.println("Las "+hora+" es de madrugada");
+			}
+		}else {
+			System.out.println("la hora " + hora+" esta en un formato incorrecto");
+		}
+		/*
+		 * if()	{
+		 * }
+		 * */
+		
+		//MI POSIBLE SOLUCION SIN ACABAR
+		/*
+		Scanner teclado = new Scanner(System.in);
+		int horasConvertidas=0;
+		String horas="";
+		String tiempo="";
+		do {
+		System.out.println("INTRODUCE LA HORA");
+		teclado.nextLine();
+		horas=tiempo.substring(0,2);
+		System.out.println(horas);
+		horasConvertidas=Integer.parseInt(horas);
+		}while(horasConvertidas>=00 && horasConvertidas<=23 ==false);
+		
+		if(horasConvertidas>=06 && horasConvertidas<=11)	{
+			System.out.println("Las "+horas+" es por la mañana");
+		}
+		else if(horasConvertidas>=12 && horasConvertidas<=19) {
+			System.out.println("Las "+tiempo+" es por la tarde");
+		}
+		else if(horasConvertidas>=20 && horasConvertidas<=23) {
+			System.out.println("Las "+tiempo+" es por la noche");
+		}
+		else if(horasConvertidas>=00 && horasConvertidas<=05) {
+			System.out.println("Las "+tiempo+" es de madrugada");
+		}
+		else if(horasConvertidas>24) {
+			System.out.println("ERROR ESE NUMERO NO CORRESPONDE A LAS HORAS");
+		}
+		*/
+//		for(int i=0;i<tiempo.length();i++) {
+//		}
+//		
 	}
 }
 
