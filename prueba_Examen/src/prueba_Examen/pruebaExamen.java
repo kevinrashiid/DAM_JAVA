eb843a4063af3b5766f7926c9ef741ca0a1f0cdf
kevin <kevin.villaroel@educa.madrid.org>
2025-10-24 12:39:22 +0200

    otros

--- prueba_Examen/src/prueba_Examen/pruebaExamen.java
+++ prueba_Examen/src/prueba_Examen/pruebaExamen.java
@@ -14,23 +14,57 @@ public class pruebaExamen {
 		String letra=teclado.nextLine();
 		String espacio=" ";
 		int contador = 0;
-		boolean bandera = false;
-		do {
-			for (int i = 0; i < frase.length(); i++) {
-				if (frase.charAt(i) == letra.charAt(0)) {
-					System.out.println(letra.charAt(0));
-				}else if(frase.charAt(i)==espacio.charAt(0)) {
-						System.out.println(" ");
-				}else {
-					System.out.println("*");
-				}
-				System.out.println("INTRODUCE OTRA LETRA--> ");
-				letra = teclado.nextLine();
+		boolean fraseTerminada = false;
+		String fraseAhorcado="";
+		/*
+		//me faltaba esto
+//		do {
+		for (int i = 0; i < frase.length(); i++) {
+//			if(frase.charAt(i) == espacio.charAt(0)) { //lo que hice yo
+//				System.out.print(' ');
+//			}
+			if (frase.charAt(i) == letra.charAt(0) || frase.charAt(i)==' ') {
+				fraseAhorcado=fraseAhorcado+frase.charAt(i);
+			} else {
+				fraseAhorcado=fraseAhorcado+'*';
 			}
-			contador++; //para contar los intentos
-		} while (bandera == false);
+		}
+		System.out.println("Resultado " +fraseAhorcado);
+
+//			contador++; //para contar los intentos
+//		} while (bandera == false);
+		*/
+		//EJERCICIO 2
+		String fraseNueva="";
+		int contadorLetra=0;
+		while (fraseTerminada == false) {
+			int contadorAsteriscos=0;
+			System.out.print("INTRODUCE LETRA--> ");
+			letra=teclado.nextLine();
+			for (int i = 0; i < frase.length(); i++) {
+				if (frase.charAt(i) == letra.charAt(0) || frase.charAt(i)==' ') {
+					fraseNueva=fraseNueva+frase.charAt(i);
+				} else {
+					fraseNueva=fraseNueva+frase.charAt(i);
+				}
+				if(fraseAhorcado.charAt(i)=='*') {
+					contadorAsteriscos++;
+				}
+			}
+			
+			System.out.println("Resultado " +fraseAhorcado);
+			System.out.println("La letra "+letra+" aparece en "+contadorLetra+" ocasiones");
+				if(contadorAsteriscos==0) {
+					fraseTerminada=true;
+					System.err.println("Has necesitado "+contador+" intentos");
+				}
+				contador++; //para contar los intentos
+		};
+		
+		
 	}
 }
 
-//|| frase.charAt(i)==espacio.charAt(0)
+
+//   || frase.charAt(i)==espacio.charAt(0)
 
