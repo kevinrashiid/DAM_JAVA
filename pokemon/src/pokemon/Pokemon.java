b63bc557b332be263328304db8fc2b772624d4da
kevin <kevin.villaroel@educa.madrid.org>
2025-12-02 14:11:41 +0100

    otros

--- pokemon/src/pokemon/Pokemon.java
+++ pokemon/src/pokemon/Pokemon.java
@@ -2,38 +2,99 @@
 package pokemon;
 
 public class Pokemon {
-	
+
 	private String nombre;
 	private int codigo;
-	private String[] tipo;
-	private String evolucion;
+	private String[] tipo = new String[2]; // CREANDO ARRAY DE TAMAÑOS
+//	private String evolucion;
 	private int pv;
-	
-	//constructor de un tipo
-	public Pokemon(String name,int code,String type,String evo) {
-		this.nombre=name;
-		this.codigo=code;
-		this.tipo[0]=type;//metiendo en array de string
-		this.evolucion=evo;
-		this.pv=(int)(Math.random()*51)+50;
+	private Pokemon evolucion;
+	private int dano;
+
+	// constructor de un tipo
+	public Pokemon(int code, String name, String tipo) {
+		this.nombre = name;
+		this.codigo = code;
+		this.tipo[0] = tipo;// metiendo en array de string
+//		this.evolucion=evo;
+		this.pv = (int) (Math.random() * 51) + 50;
+		this.dano = (int) (Math.random() * 75 - 25 + 1) + 1;
 	}
-	//constructor de dos tipos
-	public Pokemon(String name,int code,String type,String type2,String evo) {
-		this.nombre=name;
-		this.codigo=code;
-	
-		this.tipo[0]=type;
-		this.tipo[1]=type2;
-		this.evolucion=evo;
-		this.pv=(int)(Math.random()*51)+50;//asignando puntos al azar
+
+	// constructor de dos tipos
+	public Pokemon(int code, String name, String type, String type2) {
+		this.nombre = name;
+		this.codigo = code;
+		this.tipo[0] = type;
+		this.tipo[1] = type2;
+//		this.evolucion=evo;
+		this.pv = (int) (Math.random() * 51) + 50;// asignando puntos al azar
+		this.dano = (int) (Math.random() * 75 - 25) + 1;
 	}
+
 	public void infoPokemon() {
-		System.out.println(this.codigo+" - "+this.nombre);
-		if(this.tipo[1]==null) {
-			System.out.println("Tipo: "+ this.tipo[0]);
-		}else {
-			System.out.println("Tipo: "+ this.tipo[0]+", "+this.tipo[1]);
+		System.out.println("~~~~INFORMACION DEL POKEMON~~~~");
+		System.out.println("CODIGO " + this.codigo + " - " + this.nombre);
+		if (this.tipo[1] == null) {
+			System.out.println("Tipo: " + this.tipo[0]);
+		} else {
+			System.out.println("Tipo: " + this.tipo[0] + ", " + this.tipo[1]);
 		}
-		System.out.println("PV: "+this.pv);
+		if (this.evolucion != null) {// si tiene evolucion
+			System.out.println("Evoluciona en: " + this.evolucion.nombre);
+		}
+		System.out.println("PV: " + this.pv);
+		System.out.println("Daño " + this.dano);
+	}
+
+	public void setEvolucion(Pokemon p) {// CAMBIANDO EL PUNTERO A OTRA VARIABLE
+		this.evolucion = p;
+	}
+
+	public Pokemon evoluciona() {
+		// PARA DARLE UN PUNTERO AL PROPIO POKEMON QUE LLAMA A ESTA FUNCION
+		Pokemon pokemon = this;
+		if (this.evolucion == null) {
+			System.out.println("Este pokemon no sabe evolucionar");
+		} else {
+			pokemon = this.evolucion;
+		}
+		return pokemon;
+	}
+
+	// FUNCION PARA HACER DAÑO
+	//HASTA QUE ALGUNO DE ELLOS LA VIDA LLEGUE A <= 0
+	public void combateContra(Pokemon atacado) {
+		// mi solucion al hacer un ataque
+//		Pokemon pokemon=this;
+//		if(pokemon.dano>=atacado.pv) {
+//			System.out.println("Pokemon derrotado");
+//		}else {
+//			System.out.println("Contrataca "+atacado+" y termina con daño de "+atacado.dano);
+//		}
+		// SOLUCION JOSE.M
+		boolean bandera=false;
+		do {
+			if (this.pv <= 0) {
+				System.out.println("Un programa sin PV no puede combatir");
+			} else if (bandera==false){
+				int danyo = (int) ((Math.random() * 51) + 25);
+				atacado.pv -= danyo;
+				if (atacado.pv > 0) {
+					danyo = (int) ((Math.random() * 51) + 25);
+					this.pv -= danyo;
+					if (this.pv > 0) {
+						System.out.println("Ningun pokemon vencio el combate");
+					} else {
+						System.out.println(this.nombre + " ha sido derrotado");
+					}
+				} else {
+					System.out.println(atacado.nombre + " ha sido derrotado");
+				}
+			}
+			if(atacado.pv<=0||this.pv<=0) {
+				bandera=true;
+			}
+		} while (bandera==false);
 	}
 }
--- pokemon/src/pokemon/ppo2.java
+++ pokemon/src/pokemon/ppo2.java
@@ -4,12 +4,32 @@ public class ppo2 {
 
 	public static void main(String[] args) {
 		//pokemon de un solo tipo
-		Pokemon gastly=new Pokemon("gastly",292,"Fastasma","Hunter");
-		//pokemon de dos tipos
+		Pokemon p1=new Pokemon(1,"Bulbasur","Planta");
+		Pokemon p2=new Pokemon(2,"Evysaur","Planta");
+		
+		
+		//Pokemons de dos tipos
+		Pokemon p3=new Pokemon(3,"charmander","fuego","Volador");
+		Pokemon p4=new Pokemon(4,"charmeleon","fuego","Volador");
+		Pokemon p5=new Pokemon(4,"charizar","fuego","dragon");
+
 
 		Pokemon[] pokedex=new Pokemon[151];//array de pokemon
 //		pokedex[0]=gastly;//añadiendo un pokemon a
+		p1.infoPokemon();//mostrar informacion del pokemon
+		p1.setEvolucion(p3);//this.evolucion=p;
+		p1.infoPokemon();
 		
-		gastly.infoPokemon();
+		//evolcionando a charmelon
+		p3.setEvolucion(p4);
+		p3.infoPokemon();
+		
+		System.err.println("####COMBATE####");
+		p2.infoPokemon();
+		p3.infoPokemon();
+		p2.combateContra(p3);
+		
 	}
+	//CAPACIDAD DE QUE LOS POKEMONS EVOLUCIONEN
+
 }
