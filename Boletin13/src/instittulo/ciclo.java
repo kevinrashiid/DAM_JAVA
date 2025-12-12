0d803945aedb98bcb52a4c5580f535081402779c
kevin <kevin.villaroel@educa.madrid.org>
2025-12-12 12:00:08 +0100

    otros

--- Boletin13/src/instittulo/ciclo.java
+++ Boletin13/src/instittulo/ciclo.java
@@ -1,48 +1,27 @@
 package instittulo;
 
 public class ciclo {
-	String nombreCiclo;
-	int año;
-	String tutor;
-	int numAlumnos;
-	String[] alumnosMatriculados;
+	private modulo[] primero=new modulo[8];
+	private modulo[] segundo=new modulo[8];
+	private String nombreCiclo;
+	private String grado;
+	
 
 	//CONSTRUCTOR
-	public ciclo(String nombre,int año,String tutor,int numeroAlumnos) {
+	public ciclo(String nombre,String grado,int año) {
 		this.nombreCiclo=nombre;
-		this.año=año;
-		this.tutor=tutor;
-		this.numAlumnos=numeroAlumnos;
+		this.grado=grado;
 	}
+	public void anyadeModulo(modulo m) {
+		
+		
+	}
+	
 	public String getNombreCiclo() {
 		return nombreCiclo;
 	}
 	public void setNombreCiclo(String nombreCiclo) {
 		this.nombreCiclo = nombreCiclo;
-	}
-	public int getAño() {
-		return año;
-	}
-	public void setAño(int año) {
-		this.año = año;
-	}
-	public String getTutor() {
-		return tutor;
-	}
-	public void setTutor(String tutor) {
-		this.tutor = tutor;
-	}
-	public int getNumAlumnos() {
-		return numAlumnos;
-	}
-	public void setNumAlumnos(int numAlumnos) {
-		this.numAlumnos = numAlumnos;
-	}
-	public String[] getAlumnosMatriculados() {
-		return alumnosMatriculados;
-	}
-	public void setAlumnosMatriculados(String[] alumnosMatriculados) {
-		this.alumnosMatriculados = alumnosMatriculados;
 	}
 	
 	
